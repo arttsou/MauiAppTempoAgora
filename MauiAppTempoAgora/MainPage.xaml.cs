aa839f38bd97f8556478d63c9ccab02e6e004acb
Arthur S <mackney040@gmail.com>
2026-04-05 12:47:33 -0300

    App Do Tempo

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace MauiAppTempoAgora
+﻿using MauiAppTempoAgora.Models;
+using MauiAppTempoAgora.Services;
+
+namespace MauiAppTempoAgora
 {
     public partial class MainPage : ContentPage
     {
@@ -9,16 +12,45 @@
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object? sender, EventArgs e)
+        private async void Button_Clicked(object sender, EventArgs e)
         {
-            count++;
+            try
+            {
+                if(!string.IsNullOrEmpty(txt_cidade.Text))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
+                    if (t != null)
+                    {
+                        string dados_previsao = "";
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+                        dados_previsao = $"Latitude: {t.lat}\n" +
+                                         $"Longitude: {t.lon}\n" +
+                                         $"Condição: {t.main} - {t.description}\n" +
+                                         $"Nascer do Sol: {t.sunrise}\n" +
+                                         $"Pôr do Sol: {t.sunset}\n" +
+                                         $"Temp Máx: {t.temp_max}°C\n" +
+                                         $"Velocidade do Vento: {t.speed} m/s\n" +
+                                         $"Visibilidade: {t.visibility} m\n" +
+                                         $"Temp Min: {t.temp_min}°C\n";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+
+                        lbl_res.Text = dados_previsao;
+                    }
+                    else
+                    {
+                        lbl_res.Text = "Não foi possível obter a previsão para a cidade informada. Verifique o nome e tente novamente.";    
+                    }
+                }
+                else
+                {
+                    lbl_res.Text= "Por favor, insira o nome de uma cidade.";
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "OK");
+            }
+
         }
     }
 }
---
+++ MauiAppTempoAgora/Services/DataService.cs
@@ -0,0 +1,51 @@
+﻿using MauiAppTempoAgora.Models;
+using Newtonsoft.Json.Linq;
+
+namespace MauiAppTempoAgora.Services
+{
+    public class DataService
+    {
+        public static async Task<Tempo?> GetPrevisao(string cidade)
+        {
+            Tempo? t = null;
+
+            string chave = "e7b1fe40983ad18df5a9a1df00b74ae4";
+
+            string url = $"https://api.openweathermap.org/data/2.5/weather?" +
+                         $"q={cidade}&appid={chave}&units=metric&lang=pt_br";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage resp = await client.GetAsync(url);
+
+                if (resp.IsSuccessStatusCode) 
+                { 
+                    string json = await resp.Content.ReadAsStringAsync();
+
+                    var rascunho = JObject.Parse(json);
+
+                    DateTime time = new();
+                    DateTime sunrise = time.AddSeconds((double)rascunho["sys"]["sunrise"]).ToLocalTime();
+                    DateTime sunset = time.AddSeconds((double)rascunho["sys"]["sunset"]).ToLocalTime();
+
+                    t = new ()
+                    {
+                        lon = (double)rascunho["coord"]["lon"],
+                        lat = (double)rascunho["coord"]["lat"],
+                        temp_min = (double)rascunho["main"]["temp_min"],
+                        temp_max = (double)rascunho["main"]["temp_max"],
+                        sunrise = sunrise.ToString(),
+                        sunset = sunset.ToString(),
+                        speed = (double)rascunho["wind"]["speed"],
+                        description = (string)rascunho["weather"][0]["description"],
+                        visibility = (int)rascunho["visibility"],
+                        main = (string)rascunho["weather"][0]["main"]
+                    }; // Fecha objeto do Tempo
+                } //Fecha if do sucesso da resposta
+            }// fecha using do HttpClient
+
+            return t;
+
+        }
+    }
+}
